a62b3ae1b49d9c5e9bb986540b9fca6b74f9a13c
Andres u <urizandres@estudiantec.cr>
2024-02-15 19:47:47 -0600

    +testbench y parte de seleccion valor inicial

--- laboratorio_1/Problema3/contador_regresivo.sv
+++ laboratorio_1/Problema3/contador_regresivo.sv
@@ -1,10 +1,27 @@
-module contador_regresivo(input clk, reset,
-									output reg[3:0] out);
+module contador_regresivo(input clk, reset, dec, inc,
+									output reg[5:0] out);
 								  
-always @ (posedge clk) begin
-	if (!reset)
-		out <= 0;
-	else
-		out <= out +1;
-	end
+//always @ (posedge clk) begin
+//	if (!reset)
+//		out <= 9;
+//	
+//	else begin
+////		if (out == 0) begin
+////		end
+////		
+////		else
+////			out <= out - 1;
+//	
+//	end
+//end
+
+always @ (dec, inc) begin
+	if (!dec)
+		out <= out - 1;
+		
+	if (!inc)
+		out <= out + 1;
+	
+end
+	
 endmodule

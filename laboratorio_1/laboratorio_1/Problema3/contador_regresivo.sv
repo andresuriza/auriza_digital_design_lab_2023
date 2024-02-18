64bc9d78f5ffadd889eea6bc6db704fab15e9fb1
Andres u <urizandres@estudiantec.cr>
2024-02-17 18:07:15 -0600

    Testbench funciona, fpga sin decrease funcional

--- laboratorio_1/laboratorio_1/Problema3/contador_regresivo.sv
+++ laboratorio_1/laboratorio_1/Problema3/contador_regresivo.sv
@@ -3,36 +3,38 @@ module contador_regresivo(input logic[5:0]num,
 									input logic dec,
 								   output logic[5:0] out,
 								   output logic[6:0] seg);
-																										
-always @ (reset, dec) begin
+
+always_comb begin
 	case (out)
-		0 : seg = 7'b0000001;
-		1 : seg = 7'b1001111;
-		2 : seg = 7'b0010010;
-		3 : seg = 7'b0000110;
-		4 : seg = 7'b1001100;
-		5 : seg = 7'b0100100;
-		6 : seg = 7'b0100000;
-		7 : seg = 7'b0001111;
-		8 : seg = 7'b0000000;
-		9 : seg = 7'b0000100;
-		10 : seg = 7'b0001000;
-		11 : seg = 7'b1100000;
-		12 : seg = 7'b0110001;
-		13 : seg = 7'b1000010;
-		14 : seg = 7'b0110000;
-		15 : seg = 7'b0111000;
-		default: seg = 7'b1111111;
+			0 : seg = 7'b0000001;
+			1 : seg = 7'b1001111;
+			2 : seg = 7'b0010010;
+			3 : seg = 7'b0000110;
+			4 : seg = 7'b1001100;
+			5 : seg = 7'b0100100;
+			6 : seg = 7'b0100000;
+			7 : seg = 7'b0001111;
+			8 : seg = 7'b0000000;
+			9 : seg = 7'b0000100;
+			10 : seg = 7'b0001000;
+			11 : seg = 7'b1100000;
+			12 : seg = 7'b0110001;
+			13 : seg = 7'b1000010;
+			14 : seg = 7'b0110000;
+			15 : seg = 7'b0111000;
+			default: seg = 7'b1111111;
 	endcase
+end
+									
+always @ (reset, dec) begin
+
 		
-	if (!reset) begin
+	if (reset == 0)
 		out = num;
-	end
-	else begin
-		if (out != 0) begin
-			if (!dec)
-				out <= out -1;
-		end
+
+	if (dec == 0) begin
+		//if (out >= 0)
+		out <= out -1;
 	end
 end
 	

6a85d061bddef8a4cafb6a180c8737dad3016a41
Dylan Garbanzo <dgarbanzof@estudiantec.cr>
2024-02-19 09:27:39 -0600

    V2 Problema2
    falta probar en FPGA

--- Laboratorio2/Problema2.vhd
+++ Laboratorio2/Problema2.vhd
@@ -1,8 +1,7 @@
--- File: Problema2.vhdl
+-- 1-bit full adder
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- 1-bit full adder
 entity full_adder is
     Port ( a : in STD_LOGIC;
            b : in STD_LOGIC;
@@ -17,14 +16,49 @@ begin
     cout <= (a and b) or (cin and (a xor b));
 end Behavioral;
 
--- 4-bit adder using 1-bit adders
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+-- 4-bit binary to 7-segment hexadecimal decoder
+entity hex_decoder is
+    Port ( bin : in STD_LOGIC_VECTOR (3 downto 0);
+           hex : out STD_LOGIC_VECTOR (6 downto 0));
+end hex_decoder;
+
+architecture Behavioral of hex_decoder is
+begin
+    process(bin)
+    begin
+        case bin is
+            when "0000" => hex <= "1000000"; -- 0
+            when "0001" => hex <= "1111001"; -- 1
+            when "0010" => hex <= "0100100"; -- 2
+            when "0011" => hex <= "0110000"; -- 3
+            when "0100" => hex <= "0011001"; -- 4
+            when "0101" => hex <= "0010010"; -- 5
+            when "0110" => hex <= "0000010"; -- 6
+            when "0111" => hex <= "1111000"; -- 7
+            when "1000" => hex <= "0000000"; -- 8
+            when "1001" => hex <= "0010000"; -- 9
+            when "1010" => hex <= "0001000"; -- A
+            when "1011" => hex <= "0000011"; -- B
+            when "1100" => hex <= "1000111"; -- C
+            when "1101" => hex <= "0100001"; -- D
+            when "1110" => hex <= "0000110"; -- E
+            when "1111" => hex <= "0001110"; -- F
+            when others => hex <= "1111111"; -- Error
+        end case;
+    end process;
+end Behavioral;
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+-- 4-bit adder using 1-bit adders and 7-segment display
 entity Problema2 is
     Port ( a : in STD_LOGIC_VECTOR (3 downto 0);
            b : in STD_LOGIC_VECTOR (3 downto 0);
-           sum : out STD_LOGIC_VECTOR (3 downto 0);
-           cout : out STD_LOGIC);
+           sum : buffer STD_LOGIC_VECTOR (3 downto 0);
+           cout : out STD_LOGIC;
+           hex : out STD_LOGIC_VECTOR (6 downto 0));
 end Problema2;
 
 architecture Behavioral of Problema2 is
@@ -35,6 +69,10 @@ architecture Behavioral of Problema2 is
                sum : out STD_LOGIC;
                cout : out STD_LOGIC);
     end component;
+    component hex_decoder
+        Port ( bin : in STD_LOGIC_VECTOR (3 downto 0);
+               hex : out STD_LOGIC_VECTOR (6 downto 0));
+    end component;
     signal c : STD_LOGIC_VECTOR (4 downto 0);
 begin
     FA0: full_adder port map (a(0), b(0), '0', sum(0), c(1));
@@ -42,57 +80,5 @@ begin
     FA2: full_adder port map (a(2), b(2), c(2), sum(2), c(3));
     FA3: full_adder port map (a(3), b(3), c(3), sum(3), c(4));
     cout <= c(4);
-end Behavioral;
-
--- Hexadecimal to 7-segment decoder
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-entity hex_to_7seg is
-    Port ( hex : in STD_LOGIC_VECTOR (3 downto 0);
-           seg : out STD_LOGIC_VECTOR (6 downto 0));
-end hex_to_7seg;
-
-architecture Behavioral of hex_to_7seg is
-begin
-    process(hex)
-    begin
-        case hex is
-            when "0000" => seg <= "1000000"; -- 0
-            when "0001" => seg <= "1111001"; -- 1
-            when "0010" => seg <= "0100100"; -- 2
-            when "0011" => seg <= "0110000"; -- 3
-            when "0100" => seg <= "0011001"; -- 4
-            when "0101" => seg <= "0010010"; -- 5
-            when "0110" => seg <= "0000010"; -- 6
-            when "0111" => seg <= "1111000"; -- 7
-            when "1000" => seg <= "0000000"; -- 8
-            when "1001" => seg <= "0010000"; -- 9
-            when "1010" => seg <= "0001000"; -- A
-            when "1011" => seg <= "0000011"; -- B
-            when "1100" => seg <= "1000110"; -- C
-            when "1101" => seg <= "0100001"; -- D
-            when "1110" => seg <= "0000110"; -- E
-            when "1111" => seg <= "0001110"; -- F
-            when others => seg <= "1111111"; -- Error
-        end case;
-    end process;
-end Behavioral;
-
--- Top-level entity
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-entity top_level is
-    Port ( a : in STD_LOGIC_VECTOR (3 downto 0);
-           b : in STD_LOGIC_VECTOR (3 downto 0);
-           seg : out STD_LOGIC_VECTOR (6 downto 0);
-           led : out STD_LOGIC);
-end top_level;
-
-architecture Behavioral of top_level is
-    signal sum : STD_LOGIC_VECTOR (3 downto 0);
-    signal cout : STD_LOGIC;
-begin
-    U1: entity work.Problema2 port map (a => a, b => b, sum => sum, cout => cout);
-    U2: entity work.hex_to_7seg port map (hex => sum, seg => seg);
-    led <= cout;
+    HD: hex_decoder port map (sum, hex);
 end Behavioral;

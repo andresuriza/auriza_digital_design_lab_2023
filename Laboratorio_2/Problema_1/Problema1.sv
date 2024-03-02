744d1923c76d5ad87c502e3840c97c3cfac0f813
Dylan Garbanzo <dgarbanzof@estudiantec.cr>
2024-03-02 16:35:20 -0600

    implementación Substractor modular

--- Laboratorio_2/Problema_1/Problema1.sv
+++ Laboratorio_2/Problema_1/Problema1.sv
@@ -46,6 +46,53 @@ module adder_n_bit #(parameter N = 4)(
     assign cout = c[N-1];
 endmodule
 
+//Half - subtractor -----------
+module half_subtractor (
+    input bit a,
+    input bit b,
+    output difference,
+    output borrow
+);
+    assign difference = a ^ b;
+    assign borrow = (~a) & b;
+endmodule
+
+
+//Full - subtractor -----------
+module full_subtractor (
+    input bit a,
+    input bit b,
+    input bit bin,
+    output difference,
+    output bout
+);
+    wire d1, b1, b2;
+    half_subtractor HS1(.a(a), .b(b), .difference(d1), .borrow(b1));
+    half_subtractor HS2(.a(d1), .b(bin), .difference(difference), .borrow(b2));
+    assign bout = b1 | b2;
+endmodule
+
+
+//Nbit - subtractor -----------
+module subtractor_n_bit #(parameter N = 4)(
+    input [N-1:0] a,
+    input [N-1:0] b,
+    output [N-1:0] difference,
+    output bout
+);
+    wire [N:0] b_;
+    genvar i;
+    generate
+        for (i = 0; i < N; i = i + 1) begin : SUB_LOOP
+            if (i == 0) begin
+                full_subtractor FS1(.a(a[i]), .b(b[i]), .bin(1'b0), .difference(difference[i]), .bout(b_[i]));
+            end else begin
+                full_subtractor FS(.a(a[i]), .b(b[i]), .bin(b_[i-1]), .difference(difference[i]), .bout(b_[i]));
+            end
+        end
+    endgenerate
+    assign bout = b_[N-1];
+endmodule
 
 //Multiplier -----------
 module multiplicador #(parameter N = 8)(input [N-1:0] a, b, output reg [2*N-1:0] product);
@@ -75,30 +122,18 @@ module Problema1 #(parameter WIDTH = 4) (
 
 logic [2 * (WIDTH - 1):0] multResult;
 logic [WIDTH-1:0] addResult;
+logic [WIDTH-1:0] subResult;
 logic addCarry;
 
 adder_n_bit #(WIDTH) adder(.a(A), .b(B), .sum(addResult), .cout(addCarry));
+subtractor_n_bit #(WIDTH) subtractor(.a(A), .b(B), .difference(subResult), .bout());
 multiplicador #(WIDTH) mult (.a(A), .b(B), .product(multResult));
 to_7seg BCDseg(result, seg1, seg2);
 						
     always_comb begin
         case (opcode)
-            4'b0000: // ADD
-            begin
-                result = addResult;
-            end
-            4'b0001: // SUB
-                begin
-                    logic [WIDTH-1:0] borrow;
-                    borrow = ~A & B;
-                    result = A ^ B;
-                    while(borrow != 0) begin
-                        logic [WIDTH-1:0] shiftedBorrow;
-                        shiftedBorrow = borrow << 1;
-                        borrow = ~result & shiftedBorrow;
-                        result = result ^ shiftedBorrow;
-                    end
-                end
+            4'b0000: result = addResult;  // ADD
+            4'b0001: result = subResult;  // SUB
             4'b0010: result = multResult;	// MUL
             4'b0011: result = A / B; // DIV
             4'b0100: result = A % B; // MOD

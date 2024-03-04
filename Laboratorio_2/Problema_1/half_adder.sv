669cb1360706a84208d2fd331235b0e0c23a76d9
Andres u <urizandres@estudiantec.cr>
2024-03-04 16:35:34 -0600

    Eliminacion modulos innecesarios

--- Laboratorio_2/Problema_1/half_adder.sv
+++
@@ -1,8 +0,0 @@
-// Es un full adder
-module half_adder (input logic a, b,
-												 input logic cin,
-												 output logic sum, cout);
-
-		assign sum = (a ^ b) ^ cin;
-		assign cout = (cin & (a ^ b)) | (a & b);
-endmodule

9d80548c74251d30a165a93880bd610d1c3b8da7
Dylan Garbanzo <dgarbanzof@estudiantec.cr>
2024-03-04 11:21:40 -0600

    Arreglos FLAGS

--- Laboratorio_2/Problema_1/multiplier.sv
+++ Laboratorio_2/Problema_1/multiplier.sv
@@ -1,20 +1,24 @@
 module multiplier #(parameter N = 4)(
-	input [N-1:0] a, b, 
-	output reg [2*N-1:0] product);
-	
+    input [N-1:0] a, b, 
+    output reg [2*N-1:0] product
+);
 	
     integer i, j;
     reg [2*N-1:0] temp;
-    always @* begin
+    
+    always @* begin        
+        // Initialize product
         product = 0;
+        
         for (i = 0; i < N; i = i + 1) begin
             if (b[i]) begin
-                temp = a << i; //a * 2i
+                temp = a << i; // a * 2^i
+                
                 for (j = 0; j < 2*N; j = j + 1) begin
                     if (product[j] & temp[j]) begin
-                        product[j+1] = product[j+1] ^ 1; //add carry
+                        product[j+1] = product[j+1] ^ 1; // Add carry
                     end
-                    product[j] = product[j] ^ temp[j]; // add without carry
+                    product[j] = product[j] ^ temp[j]; // Add without carry
                 end
             end
         end

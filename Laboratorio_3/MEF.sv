dc883c4478dc8d5e84ca6771487497230530cb0b
IsaCorQ <85208919+IsaCorQ@users.noreply.github.com>
2024-04-19 06:48:53 -0600

    Cambio a mef

--- Laboratorio_3/MEF.sv
+++ Laboratorio_3/MEF.sv
@@ -1,7 +1,7 @@
 module MEF (input logic btn_nxt, m, rst, clk, [2:0] bcs, 
 				output logic [7:0] estado);
 
-	logic [3:0] state, next_state;
+	logic [2:0] state, next_state;
 	logic t0, lp;
 	logic [7:0] mCounter;
 	reg [24:0] up_pst;
@@ -12,7 +12,7 @@ module MEF (input logic btn_nxt, m, rst, clk, [2:0] bcs,
 	//actual state logic
 	always_ff @ (posedge clk or posedge rst) begin
 		if (rst) begin
-			state = 4'b0000;
+			state = 3'b000;
 			mCounter = 'h0;
 		end
 		
@@ -32,41 +32,35 @@ module MEF (input logic btn_nxt, m, rst, clk, [2:0] bcs,
 	//next state logic
 	always_comb 
 		case(state)
-				4'b0000: 
-					if (x) next_state = 4'b0001; 
-					else next_state = 4'b0000; //inicio
-				4'b0001: 
+				3'b000: 
+					if (x) next_state = 3'b001; 
+					else next_state = 3'b000; //inicio
+				3'b001: 
 					begin
 						regBarcos registroBarcos( bcs,nw_pst, clk, up_pst)
-						if (x) next_state = 4'b0001; 
-						else next_state = 4'b0010; //barco
+						if (x) next_state = 3'b010; 
+						else next_state = 3'b001; //barco
 					end
-				4'b0010: 
+				3'b010: 
 					begin//Jugador
 						selecBarcos SeleccionarBarcos()
-						if (t0) next_state = 4'b0111; 
-						else if (x) next_state = 4'b0100;
-						else next_state = 4'b0011;
+						if (t0) next_state = 3'b100; 
+						else if (x) next_state = 3'b011;
+						else next_state = 3'b010;
 					end
-				4'b0011: 
-					next_state = 4'b0101;//Revisa casilla
-				4'b0101: 
-					next_state = 4'b0110;//Revisa si pego barco
-				4'b0110: 
-					if (lp) next_state = 4'b1100; 
-					else next_state = 4'b0111;//Revisa cuantos barcos quedan
-				4'b0111: 
-					next_state = 4'b1000;//PC
-				4'b1000: 
-					next_state = 4'b1001;//Casilla PC
-				4'b1001: 
-					next_state = 4'b1010;//Revision barco PC
-				4'b1011: 
-					if (lp) next_state = 4'b1100; 
-					else next_state = 4'b0011;//Revision cuantos barcos quedan PC
-				4'b1100: 
-					next_state = 4'b1100;		
-				default: next_state = 4'b0000;
+				3'b011: 
+					verifBarco verifDisparo()
+					if (lp) next_state = 3'b110; 
+					else next_state = 3'b100;//Revisa cuantos barcos quedan
+				3'b100: 
+					next_state = 3'b101;//PC
+				3'b101:
+					verifDisp verifDisparo()A
+					if (lp) next_state = 3'b110; 
+					else next_state = 3'b010;//Revision cuantos barcos quedan PC
+				3'b110: 
+					next_state = 3'b110;		
+				default: next_state = 3'b000;
 		endcase
 	
 endmodule
